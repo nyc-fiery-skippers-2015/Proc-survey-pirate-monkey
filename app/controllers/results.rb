a31ba85669111d67c1dedfd9eb316a658f031ebf
Mendel Schneerson <benlights@users.noreply.github.com>
2015-05-31 11:34:16 -0400

    adds controller routes for results

--- app/controllers/results.rb
+++ app/controllers/results.rb
@@ -1,9 +1,15 @@
 get '/surveys/:id/results' do
+  current_survey = Survey.find_by(id: params[:id])
+  erb :'results/show', locals: {survey: current_survey}
 end
 
 get 'surveys/:survey_id/:user_id' do
+  current_survey = Survey.find_by(id: params[:id])
+  user = User.find_by(id: params[:user_id])
+  erb :'results/users', locals: {survey: current_survey, user: user}
 end
 
 get '/surveys/:id/takers' do
+  current_survey = Survey.find_by(id: params[:id])
+  erb :'results/takers', locals: {survey: current_survey}
 end
-
