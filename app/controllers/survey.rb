0d7938a760cfdc29d51c16d1881a28e100c086ed
Nikolai Calder <redlacialokin@gmail.com>
2015-05-31 15:58:46 -0400

    modified controller route,views, and query methods

--- app/controllers/survey.rb
+++ app/controllers/survey.rb
@@ -1,5 +1,5 @@
 get '/survey/:id' do
-  survey = Survey.find(params[:id])
+  survey = Survey.find_by(id: params[:id])
   erb :"survey/show", locals: {survey: survey}
 end
 
@@ -7,21 +7,21 @@ get '/surveys/new' do
   erb :'surveys/new'
 end
 
-post '/surveys/new'  do
+post '/surveys'  do
   survey = Survey.new( params[:survey])
-  survey.creator_id = 1
+  survey.creator = currently_logged_in
   return [500, "Something went wrong"] unless survey.save
   redirect "/surveys/#{survey.id}/questions"
 end
 
 get '/surveys/:id/questions' do
   current_survey = Survey.find_by(id: params[:id])
-  erb :'surveys/questions', locals:{current_survey: current_survey}
+  erb :'surveys/questions', locals:{survey: current_survey}
 end
 
 post '/surveys/:id/questions' do
   question = Question.new(params[:question])
-  question.survey_id = params[:id]
+  question.survey = Survey.find_by(id: params[:id])
   return [500, "Something went wrong"] unless question.save
   redirect "/questions/#{question.id}/answers"
 end
