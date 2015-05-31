31778ac5bad55ecff7cbd3e6b9dda363de1f21e9
Mendel Schneerson <benlights@users.noreply.github.com>
2015-05-31 16:42:17 -0400

    fixes controller

--- app/controllers/answers.rb
+++ app/controllers/answers.rb
@@ -8,7 +8,7 @@ post '/questions/:id/answers' do
   new_answer = Answer.new(text: params[:text])
   current_question = Question.find_by(id: params[:id])
   new_answer.question = current_question
-  new_answer.user = currently_logged_in
+  new_answer.creator = currently_logged_in
   # this has to changed into ajax
   return [500, "This Question is Not Valid"] unless new_answer.save
   redirect "/questions/#{current_question.id}/answers"
