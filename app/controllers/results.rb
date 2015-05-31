703d583903aab10487aa2b968987197e6f2d4185
Mendel Schneerson <benlights@users.noreply.github.com>
2015-05-31 11:39:57 -0400

    fixes find_by method

--- app/controllers/results.rb
+++ app/controllers/results.rb
@@ -4,7 +4,7 @@ get '/surveys/:id/results' do
 end
 
 get 'surveys/:survey_id/:user_id' do
-  current_survey = Survey.find_by(id: params[:id])
+  current_survey = Survey.find_by(id: params[:survey_id])
   user = User.find_by(id: params[:user_id])
   erb :'results/users', locals: {survey: current_survey, user: user}
 end
