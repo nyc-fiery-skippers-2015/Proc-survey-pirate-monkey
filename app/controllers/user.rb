d73bfad0ce6c381261ee8c4ba7af66021fcdd05e
John Lyden <john.lyden86@gmail.com>
2015-05-31 14:11:51 -0400

    adding user controller

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,61 +1,21 @@
-get '/' do
-  redirect '/login'
+get '/users/:id' do
+  return [500, "No user with that id"] unless current_user
+  erb :'users/show', locals: {user: current_user}
 end
 
-get '/logout' do
-  session[:user_id] = nil
-  redirect '/'
-end
+get '/users/new' do
 
-get '/login' do
-  erb :'users/login'
 end
 
 post '/users' do
-  user = User.find_by(email: params[:user][:email])
-  if user.authenticate(params[:user][:password])
-    session[:user_id] = user.id
-    redirect "/users/#{user.id}"
-  else
-    redirect "/login"
-  end
-end
-
-get '/users/:id' do
-  current_user = User.find_by(id: params[:id])
-  erb :'users/_show', locals: {user: current_user}
-end
-
-get '/users/:id/edit' do
-  current_user = User.find_by(id: params[:id])
-  erb :'/users/edit', locals: {user: current_user}
-end
-
-put '/users/:id' do
-  current_user = User.find_by(id: params[:id])
-  current_user.update(params[:user])
-  return [500, 'invalid user'] unless current_user.save
-  redirect "/users/#{current_user.id}"
-end
-
-post '/users/new' do
   new_user = User.new(params[:user])
-  return [500, "invalid user"] unless new_user.save
+  return [500, "Invalid User"] unless new_user.save
   session[:user_id] = new_user.id
   redirect "/users/#{new_user.id}"
 end
 
-delete '/users/:id' do
-  current_user = User.find_by(id: params[:id])
-  return [500, "no user found"] unless current_user
+delete '/users' do
+  return [500, "No User with that ID"] unless current_user
   current_user.destroy
-  redirect '/'
-end
-
-get '/users/:id/surveys/created' do
-
-end
-
-get '/users/:id/surveys/taken' do
-
+  redirect '/login'
 end
