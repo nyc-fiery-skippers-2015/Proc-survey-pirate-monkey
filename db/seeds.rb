ab25d2cdc65bd4e14717075cbe6177a54d672f9e
John Lyden <john.lyden86@gmail.com>
2015-06-01 00:20:15 -0400

    everything functioning, surveys taken displays, only offers results button for surveys created by currently logged in user, styling is done but still meh

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,13 @@ ash = User.create(name: "Ash",email: 'ash@pokemon.com', password_hash: 'ketchum'
 snake = User.create(name: "Big Boss",email: 'metalgear@hotmail.com', password_hash: 'ketchum')
 
 survey = Survey.new(title: "Favorite Programming Language")
+survey2 = Survey.new(title: "DBC Stuff")
 
 survey.creator = ash
 survey.save
+
+survey2.creator = snake
+survey2.save
 
 question = Question.new(text: 'how old are you?')
 question.survey = survey
@@ -36,3 +40,16 @@ snake.save
 snake.answers << answer2
 snake.answers << answer
 snake.save
+
+question3 = Question.new(text: 'Favorite DBC Coach?')
+question3.survey = survey2
+question3.save
+
+answer4 = Answer.new(text: 'Zack')
+answer4.question = question3
+answer4.save
+
+answer5 = Answer.new(text: 'Chris')
+answer5.question = question3
+answer5.save
+
